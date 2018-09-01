0bdfe8fe817f98dd030cf80199b7d46e85dc8005
Raphael Oliveira <raphaelbrf@gmail.com>
2018-09-01 16:16:27 -0300

    refatoring

--- JavaScript_For_Beginners-Learn_JavaScript_From_Scratch-Udemy/exercise4/default.js
+++ JavaScript_For_Beginners-Learn_JavaScript_From_Scratch-Udemy/exercise4/default.js
@@ -2,8 +2,6 @@
 
 var name = prompt("What's your name?");
 
-document.write("Hello "+ name +" you're welcome.");
-
 var age = prompt("What's your age?");
 
-document.write(" Ok, you have "+ age);
+document.write("Hello "+ name +" you're welcome. Ok, you have "+ age);
