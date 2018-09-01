f65c4980a62c8e2197fe1887db8dd620a5e788d7
Raphael Oliveira <raphaelbrf@gmail.com>
2018-09-01 16:27:06 -0300

    make link with JS

---
+++ JavaScript_For_Beginners-Learn_JavaScript_From_Scratch-Udemy/exercise5/default.js
@@ -0,0 +1,8 @@
+
+
+var hotText = "MDN";
+var URL = "https://developer.mozilla.org/";
+
+console.log("Click to return to "+ hotText.link(URL));
+
+document.write("Click to go to "+ hotText.link(URL));
