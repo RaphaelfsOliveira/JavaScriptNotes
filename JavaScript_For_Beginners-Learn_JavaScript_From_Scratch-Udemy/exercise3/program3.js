44ea73e5598b6ec3970be8bc57acf8ffd962f389
Raphael Oliveira <raphaelbrf@gmail.com>
2018-09-01 15:12:16 -0300

    example of variables

--- JavaScript_For_Beginners-Learn_JavaScript_From_Scratch-Udemy/exercise3/program3.js
+++ JavaScript_For_Beginners-Learn_JavaScript_From_Scratch-Udemy/exercise3/program3.js
@@ -1,6 +1,11 @@
 var lives = 3;
 
 // write a variable and put inside document page
-var example = "I am a variable!.."
+var example = "I am a variable!..";
+
+document.write(example);
+
+// define the new value of variable
+var example = "This is another value of variable..";
 
 document.write(example);
