8c1ae06d96eb21b5262e1812f4747b694ab258b9
Raphael Oliveira <raphaelbrf@gmail.com>
2018-09-01 16:44:32 -0300

    coding challenge

---
+++ JavaScript_For_Beginners-Learn_JavaScript_From_Scratch-Udemy/The Variable Coding Challenge/default.js
@@ -0,0 +1,9 @@
+alert("Welcome to the Website!");
+
+var userName = prompt("What's your name?");
+
+var userColour = prompt("What's your favourite colour?");
+
+document.write("Hello "+ userName +" Your Favourite Colour is "+ userColour);
+
+console.log("Program Complete");
