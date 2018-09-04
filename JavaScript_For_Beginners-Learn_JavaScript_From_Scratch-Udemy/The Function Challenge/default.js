fcd692f1968da0620ff6d5511c8fd62d48dd2de2
Raphael Oliveira <raphaelbrf@gmail.com>
2018-09-04 01:05:21 -0300

    The Function Challenge

---
+++ JavaScript_For_Beginners-Learn_JavaScript_From_Scratch-Udemy/The Function Challenge/default.js
@@ -0,0 +1,9 @@
+
+function functionDice(number) {
+  number = Math.floor(Math.random() * number) + 1;
+  alert("Rooling Dice, the number is: "+ number);
+}
+
+functionDice(6);
+functionDice(12);
+functionDice(20);
