25b4cd3028d35849d567190024481910768cd818
Raphael Oliveira <raphaelbrf@gmail.com>
2018-09-03 23:36:51 -0300

    boolean values

--- JavaScript_For_Beginners-Learn_JavaScript_From_Scratch-Udemy/exercise9/default.js
+++ JavaScript_For_Beginners-Learn_JavaScript_From_Scratch-Udemy/exercise9/default.js
@@ -1,1 +1,18 @@
 
+var correctAnswer = false;
+
+var answer = prompt("What is 2 + 2 ?");
+
+answer = parseInt(answer);
+
+if (answer === 4) {
+  var correctAnswer = true;
+}
+
+if (correctAnswer) {
+  alert("Good Job! The answer is indeed " + answer);
+  document.write("You are a math wiz!");
+} else {
+  alert("Sorry that is not the right answer..");
+  document.write("Please try again!");
+}
