15b6da04a1f496cc3027056defb3ab934f21752a
Raphael Oliveira <raphaelbrf@gmail.com>
2018-09-03 23:25:17 -0300

    conditional statements

--- JavaScript_For_Beginners-Learn_JavaScript_From_Scratch-Udemy/exercise8/default.js
+++ JavaScript_For_Beginners-Learn_JavaScript_From_Scratch-Udemy/exercise8/default.js
@@ -1,1 +1,12 @@
 
+var answer = prompt("What is 2 + 2 ?");
+
+answer = parseInt(answer);
+
+if (answer === 4) {
+  alert("Good job! The answer is indeed " + answer);
+  document.write("You are a math wiz!");
+} else {
+  alert("Sorry that is not right answer..");
+  document.write("Please try again!");
+}
