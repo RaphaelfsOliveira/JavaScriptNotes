5ae3195dc93d91030cf47db9c5baab1608d495b9
Raphael Oliveira <raphaelbrf@gmail.com>
2018-09-24 15:06:26 -0300

    coding challenge 4

--- the-complete-javascript-course/course-modules/2-JS-basics/final/script.js
+++ the-complete-javascript-course/course-modules/2-JS-basics/final/script.js
@@ -606,15 +606,56 @@ console.log(john.isMarried);
 */
 
 /*
-Let's remember the first coding challenge where Mark and John compared their BMIs. Let's now implement the same functionality with objects and methods.
-1. For each of them, create an object with properties for their full name, mass, and height
-2. Then, add a method to each object to calculate the BMI. Save the BMI to the object and also return it from the method.
-3. In the end, log to the console who has the highest BMI, together with the full name and the respective BMI. Don't forget they might have the same BMI.
+Let's remember the first coding challenge where Mark and John
+compared their BMIs. Let's now implement the same functionality
+with objects and methods.
 
-Remember: BMI = mass / height^2 = mass / (height * height). (mass in kg and height in meter).
+1. For each of them, create an object with properties for
+their full name, mass, and height
+2. Then, add a method to each object to calculate the BMI.
+Save the BMI to the object and also return it from the method.
+3. In the end, log to the console who has the highest BMI,
+together with the full name and the respective BMI.
+Don't forget they might have the same BMI.
+
+Remember: BMI = mass / (height * height). (mass in kg and height in meter).
 
 GOOD LUCK 😀
 */
+/*
+var mark = {
+  firstName: 'Mark',
+  lastName: 'Miller',
+  mass: 78,
+  height: 1.80,
+  fullName: function() {
+    return this.firstName +' '+ this.lastName;
+  },
+  calcBmi: function() {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  }
+}
+
+var john = {
+  firstName: 'John',
+  lastName: 'Lennon',
+  mass: 76,
+  height: 1.79,
+  fullName: function() {
+    return this.firstName +' '+ this.lastName;
+  },
+  calcBmi: function() {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  }
+}
+console.log(mark.calcBmi(), john.calcBmi());
+
+var higherBMI = mark.calcBmi() > john.calcBmi() ? mark.fullName() +' '+ parseInt(mark.bmi) : john.fullName() +' '+ parseInt(john.bmi);
+console.log('The Higher BMI is: '+ higherBMI);
+*/
+
 /*
 var john = {
     fullName: 'John Smith',
