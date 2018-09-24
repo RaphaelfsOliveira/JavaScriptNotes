703a871a5a16ce5397f1159d5e36123d912e5500
Raphael Oliveira <raphaelbrf@gmail.com>
2018-09-24 01:41:55 -0300

    course modules

---
+++ the-complete-javascript-course/course-modules/2-JS-basics/starter/default.js
@@ -0,0 +1,15 @@
+console.log("Hello World");
+
+var firstName = "Jack";
+var lastName = "Smith";
+var age = 28;
+var job;
+
+document.write(firstName+"<br>");
+document.write(lastName+"<br>");
+document.write(age+"<br>");
+
+document.write(job+"<br>");
+
+job = 'Teacher'
+document.write(job+"<br>");
