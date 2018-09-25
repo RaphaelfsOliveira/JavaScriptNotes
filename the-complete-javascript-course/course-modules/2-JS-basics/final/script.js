36b5923d0b9c755672b7c47e74e943e493df6b57
Raphael Oliveira <raphaelbrf@gmail.com>
2018-09-25 01:24:32 -0300

    Coding Challenge 5

--- the-complete-javascript-course/course-modules/2-JS-basics/final/script.js
+++ the-complete-javascript-course/course-modules/2-JS-basics/final/script.js
@@ -692,7 +692,7 @@ if (john.calcBMI() > mark.calcBMI()) {
 * Loops and iteration
 */
 
-
+/*
 // for loop
 for (var i = 1; i <= 20; i += 1) {
     console.log(i);
@@ -741,7 +741,7 @@ for (var i = 0; i < john.length; i++) {
 for (var i = john.length - 1; i >= 0; i--) {
     console.log(john[i]);
 }
-
+*/
 
 
 
@@ -750,28 +750,130 @@ for (var i = john.length - 1; i >= 0; i--) {
 */
 
 /*
-Remember the tip calculator challenge? Let's create a more advanced version using everything we learned!
+Remember the tip calculator challenge? Let's create a more advanced
+version using everything we learned!
 
-This time, John and his family went to 5 different restaurants. The bills were $124, $48, $268, $180 and $42.
-John likes to tip 20% of the bill when the bill is less than $50, 15% when the bill is between $50 and $200, and 10% if the bill is more than $200.
+This time, John and his family went to 5 different restaurants.
+The bills were $124, $48, $268, $180 and $42.
+John likes to tip 20% of the bill when the bill
+is less than $50, 15% when the bill is between $50 and $200,
+and 10% if the bill is more than $200.
 
 Implement a tip calculator using objects and loops:
 1. Create an object with an array for the bill values
 2. Add a method to calculate the tip
-3. This method should include a loop to iterate over all the paid bills and do the tip calculations
-4. As an output, create 1) a new array containing all tips, and 2) an array containing final paid amounts (bill + tip). HINT: Start with two empty arrays [] as properties and then fill them up in the loop.
+3. This method should include a loop to iterate over all the
+paid bills and do the tip calculations
+4. As an output, create 1) a new array containing all tips, and 2)
+an array containing final paid amounts (bill + tip).
+HINT: Start with two empty arrays [] as properties and then
+fill them up in the loop.
 
 
-EXTRA AFTER FINISHING: Mark's family also went on a holiday, going to 4 different restaurants. The bills were $77, $375, $110, and $45.
-Mark likes to tip 20% of the bill when the bill is less than $100, 10% when the bill is between $100 and $300, and 25% if the bill is more than $300 (different than John).
+EXTRA AFTER FINISHING: Mark's family also went on a holiday,
+going to 4 different restaurants. The bills were $77, $375, $110, and $45.
+Mark likes to tip 20% of the bill when the bill is less than $100,
+10% when the bill is between $100 and $300, and 25% if the bill
+is more than $300 (different than John).
 
-5. Implement the same functionality as before, this time using Mark's tipping rules
-6. Create a function (not a method) to calculate the average of a given array of tips. HINT: Loop over the array, and in each iteration store the current sum in a variable (starting from 0). After you have the sum of the array, divide it by the number of elements in it (that's how you calculate the average)
+5. Implement the same functionality as before, this time using
+Mark's tipping rules
+6. Create a function (not a method) to calculate the
+average of a given array of tips. HINT: Loop over the array,
+and in each iteration store the current sum in a variable
+(starting from 0). After you have the sum of the array,
+divide it by the number of elements in it (that's how you calculate the average)
 7. Calculate the average tip for each family
 8. Log to the console which family paid the highest tips on average
 
 GOOD LUCK 😀
 */
+
+var accounts = {
+  bills: [124, 48, 268, 180, 42],
+  paidBills: [],
+  paidTips: [],
+  avgTips: 0,
+  calcBills: function(bill) {
+    return bill + this.calcTips(bill);
+  },
+  calcTips: function(bill) {
+    var tip;
+    if (bill > 200) {
+      tip = bill * 0.1;
+    } else if (bill >= 50) {
+      tip = bill * 0.15;
+    } else {
+      tip = bill * 0.2;
+    }
+    return tip;
+  },
+  saveBills: function() {
+    for (var i = 0; i < this.bills.length; i++) {
+      var paidBill = this.calcBills(this.bills[i]);
+      this.paidBills.push(paidBill);
+    }
+  },
+  saveTips: function() {
+    for (var i = 0; i < this.bills.length; i++) {
+      var paidTip = this.calcTips(this.bills[i]);
+      this.paidTips.push(paidTip);
+    }
+  },
+  removeBillsTips: function(qtyRemove) {
+    for (var i = 0; i < qtyRemove; i++) {
+      this.paidBills.pop();
+      this.paidTips.pop();
+    }
+  },
+  calcAvgTips: function() {
+    if (this.paidTips.length > 0) {
+      for (var i = 0; i < this.paidTips.length; i++) {
+        this.avgTips += this.paidTips[i];
+      }
+      this.avgTips /= this.paidTips.length;
+    } else {
+      alert("Calc paid tips first", this.paidTips);
+    }
+  },
+  calcAll: function() {
+    this.saveBills();
+    this.saveTips();
+    console.log('All saved');
+    this.calcAvgTips();
+    console.log('calculating avg Tips...');
+    console.log('Done.');
+  },
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*
 var john = {
