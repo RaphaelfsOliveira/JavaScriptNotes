939c66caf08258045efae97150ba5d03ddf802ad
Raphael Oliveira <raphaelbrf@gmail.com>
2018-10-02 23:38:31 -0300

    Primitives vs Objects

--- the-complete-javascript-course/course-modules/5-advanced-JS/starter/script.js
+++ the-complete-javascript-course/course-modules/5-advanced-JS/starter/script.js
@@ -42,6 +42,7 @@ console.log(jane.lastName);
 
 
 // Object.create
+/*
 var personProto = {
   calculateAge: function() {
     console.log(2018 - yearOfBirth);
@@ -58,6 +59,49 @@ var jane = Object.create(personProto, {
   yearOfBirth: {value: 1969},
   job: {value: 'designer'}
 });
+*/
+
+
+// Primitives vs Objects
+
+// Primitives
+/*
+var a = 23;
+var b = a;
+a = 46;
+
+console.log(a);
+console.log(b);
+
+// Objects
+var obj1 = {
+  name: 'John',
+  age: 26
+}
+
+var obj2 = obj1;
+obj2.age = 30;
+
+console.log(obj1);
+console.log(obj2);
+
+// Functions
+var age = 27;
+var obj = {
+  name: 'Jonas',
+  city: 'Lisbon'
+};
+
+function change(a, b) {
+  a = 30;
+  b.city = 'San Francisco';
+}
+
+change(age, obj);
+
+console.log(age);
+console.log(obj);
+*/
 
 
 
@@ -76,4 +120,11 @@ var jane = Object.create(personProto, {
 
 
 
-var t;
+
+
+
+
+
+
+
+//
