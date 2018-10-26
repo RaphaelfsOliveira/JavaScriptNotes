d2e25a3ffbf57abde54ba22f146216f6285b438d
Raphael Oliveira <raphaelbrf@gmail.com>
2018-10-26 08:02:24 -0300

    delete some tests

--- the-complete-javascript-course/course-modules/6-budgety/starter/app.js
+++ the-complete-javascript-course/course-modules/6-budgety/starter/app.js
@@ -1,35 +1,19 @@
 var budgetController = (function() {
 
-  var x = 23;
-
-  var add = function(a) {
-    return x + a;
-  }
-
-  return {
-    publicTest: function(b) {
-      return add(b);
-    }
-  }
+  // code here
 
 })();
 
 
 var UIController = (function() {
 
-  // some code later
+  // and code here
 
 })();
 
 
 var controller = (function(budgetCtrl, UICtrl) {
 
-  var z = budgetCtrl.publicTest(5);
-
-  return {
-    anotherPublic: function() {
-      console.log(z);
-    }
-  }
+  
 
 })(budgetController, UIController);
