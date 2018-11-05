6b8a83a68b2efb76ba69ef3dc827fca12274d436
Raphael Oliveira <raphaelbrf@gmail.com>
2018-11-05 08:58:49 -0200

    percentages in UI

--- the-complete-javascript-course/course-modules/6-budgety/starter/app.js
+++ the-complete-javascript-course/course-modules/6-budgety/starter/app.js
@@ -153,7 +153,8 @@ var UIController = (function() {
     incomeLabel: '.budget__income--value',
     expensesLabel: '.budget__expenses--value',
     percentageLabel: '.budget__expenses--percentage',
-    container: '.container'
+    container: '.container',
+    expensesPercLabel: '.item__percentage'
   };
 
   return {
@@ -223,6 +224,24 @@ var UIController = (function() {
       }
     },
 
+    displayPercentages: function(percentages) {
+      var fields = document.querySelectorAll(DOMStrings.expensesPercLabel);
+
+      var nodeListForEach = function(list, callback) {
+        for (var i = 0; i < list.length; i++) {
+          callback(list[i], i);
+        }
+      };
+
+      nodeListForEach(fields, function(current, index) {
+        if (percentages[index] > 0) {
+          current.textContent = percentages[index] + '%';
+        } else {
+          current.textContent = '---';
+        }
+      });
+    },
+
   };
 
 })();
@@ -269,8 +288,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     var percentages = budgetCtrl.getPercentages();
 
     // 3. Update the UI with the new percentages
-    console.log(percentages);
-
+    UICtrl.displayPercentages(percentages);
   };
 
   var ctrlAddItem = function() {
