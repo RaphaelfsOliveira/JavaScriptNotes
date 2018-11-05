47db550c632e7663c30a41f6ffdd337b9ceb436a
Raphael Oliveira <raphaelbrf@gmail.com>
2018-11-05 08:31:40 -0200

    calculate percentages

--- the-complete-javascript-course/course-modules/6-budgety/starter/app.js
+++ the-complete-javascript-course/course-modules/6-budgety/starter/app.js
@@ -5,6 +5,19 @@ var budgetController = (function() {
     this.id = id;
     this.description = description;
     this.value = value;
+    this.percentage = -1;
+  };
+
+  Expense.prototype.calcPercentage = function(totalIncome) {
+    if (totalIncome > 0) {
+      this.percentage = Math.round((this.value / totalIncome) * 100);
+    } else {
+      this.percentage = -1;
+    }
+  };
+
+  Expense.prototype.getPercentage = function() {
+    return this.percentage;
   };
 
   var Income = function(id, description, value) {
@@ -83,6 +96,19 @@ var budgetController = (function() {
       } else {
         data.percentage = -1;
       }
+    },
+
+    calculatePercentages: function() {
+      data.allItems.exp.forEach(function(curr) {
+        curr.calcPercentage(data.totals.inc);
+      });
+    },
+
+    getPercentages: function() {
+      var allPerc = data.allItems.exp.map(function(curr) {
+        return curr.getPercentage();
+      });
+      return allPerc;
     },
 
     getBudget: function() {
@@ -237,12 +263,13 @@ var controller = (function(budgetCtrl, UICtrl) {
   var updatePercentages = function() {
 
     // 1. Calculate percentages
-
+    budgetCtrl.calculatePercentages();
 
     // 2. Read percentages from the budget controller
-
+    var percentages = budgetCtrl.getPercentages();
 
     // 3. Update the UI with the new percentages
+    console.log(percentages);
 
   };
 
