f8baff2ce522530201189f11f7be80187ab870ed
Raphael Oliveira <raphaelbrf@gmail.com>
2019-01-08 13:50:57 -0200

    // Lecture: Spread operator

--- the-complete-javascript-course/course-modules/7-ES6/starter/script.js
+++ the-complete-javascript-course/course-modules/7-ES6/starter/script.js
@@ -267,20 +267,43 @@ console.log(ages.find(e => e >= 18));
 
 /////////////////////////////////////////////////////
 // Lecture: Spread operator
+/*
+function addFourAges(a, b, c, d) {
+  return a + b + c + d;
+}
+
+var sum1 = addFourAges(18, 30, 12, 21);
+console.log(sum1);
+
+// ES5
+var ages = [18, 30, 12, 21];
+var sum2 = addFourAges.apply(null, ages);
+console.log(sum2);
+
+// ES6
+const sum3 = addFourAges(...ages);
+console.log(sum3);
+
+// essa é a expansão que o operador spread faz
+console.log(ages);
+console.log(...ages);
 
 
+const familySmith = ['John', 'Jane', 'Mark'];
+const familyMiller = ['Mary', 'Bob', 'Ann'];
+
+const bigFamily = [...familySmith, ...familyMiller];
+console.log(bigFamily);
+console.log(...bigFamily);
 
 
+const h = document.querySelector('h1');
+const boxes = document.querySelectorAll('.box');
 
+const all = [h, ...boxes];
 
-
-
-
-
-
-
-
-
+Array.from(all).forEach(e => e.style.color = 'purple');
+*/
 
 
 
