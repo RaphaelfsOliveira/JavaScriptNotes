acc67de4f13431cd8696ec670fdca60b21bcd350
Raphael Oliveira <raphaelbrf@gmail.com>
2019-01-28 06:50:21 -0200

    test

--- the-complete-javascript-course/course-modules/8-asynchronous-JS/starter/script.js
+++ the-complete-javascript-course/course-modules/8-asynchronous-JS/starter/script.js
@@ -36,11 +36,6 @@
 ///////////////////////////////////////////////////////////
 // The Old Way: Asynchronous JavaScript with Callbacks
 
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 8b7d9ec53db2fefd2cbde2fcf0a27d90d5103319
 // const getRecipe = () => {
 //   setTimeout(() => {
 //     const recipeID = [543, 795, 213, 198];
