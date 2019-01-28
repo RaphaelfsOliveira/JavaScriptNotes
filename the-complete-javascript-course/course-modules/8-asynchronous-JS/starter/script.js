adb3242fcb0653e87e11c86cc92da25faf7ebbad
Raphael Oliveira <raphaelbrf@gmail.com>
2019-01-28 06:47:13 -0200

    callback from hell rsrs tests

--- the-complete-javascript-course/course-modules/8-asynchronous-JS/starter/script.js
+++ the-complete-javascript-course/course-modules/8-asynchronous-JS/starter/script.js
@@ -1,34 +1,94 @@
-const first = () => {
-  console.log('hey there');
-  second();
-  t();
-  t3();
-  t4();
-  console.log('The End');
-}
+// const first = () => {
+//   console.log('hey there');
+//   second();
+//   t();
+//   t3();
+//   t4();
+//   console.log('The End');
+// }
+//
+// const second = () => {
+//   setTimeout(() => {
+//     console.log('Async Hey there');
+//   }, 2000);
+// }
+//
+// const t = () => {
+//   setTimeout(() => {
+//     console.log('async opaaa');
+//   }, 1);
+// }
+//
+// const t3 = () => {
+//   setTimeout(() => {
+//     console.log('async 3');
+//   }, 4000);
+// }
+//
+// const t4 = () => {
+//   setTimeout(() => {
+//     console.log('async 4');
+//   }, 2522);
+// }
+//
+// first();
 
-const second = () => {
+const getRecipe = () => {
   setTimeout(() => {
-    console.log('Async Hey there');
-  }, 2000);
-}
+    const recipeID = [543, 795, 213, 198];
+    console.log(recipeID);
 
-const t = () => {
-  setTimeout(() => {
-    console.log('async opaaa');
-  }, 1);
-}
+    setTimeout((id) => {
+      const recipe = {
+        title: 'Fresh Tomato pasta',
+        publisher: 'Jonas'
+      };
+      console.log(`${id}: ${recipe.title}`);
 
-const t3 = () => {
-  setTimeout(() => {
-    console.log('async 3');
-  }, 4000);
-}
+      setTimeout(publisher => {
+        const recipe2 = {
+          title: 'Italian Pizza',
+          publisher: 'Jonas'
+        };
+        console.log(recipe2);
+      }, 1500, recipe.publisher)
 
-const t4 = () => {
-  setTimeout(() => {
-    console.log('async 4');
-  }, 2522);
+    }, 1000, recipeID[2]);
+  }, 1500);
 }
+getRecipe();
 
-first();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// The end of world;
