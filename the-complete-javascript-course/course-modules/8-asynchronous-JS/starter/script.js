ed4af932da80cf10a983e9d1e826ed130b932d3f
Raphael Oliveira <raphaelbrf@gmail.com>
2019-01-28 06:49:09 -0200

    // The Old Way: Asynchronous JavaScript with Callbacks

--- the-complete-javascript-course/course-modules/8-asynchronous-JS/starter/script.js
+++ the-complete-javascript-course/course-modules/8-asynchronous-JS/starter/script.js
@@ -33,30 +33,53 @@
 //
 // first();
 
-const getRecipe = () => {
-  setTimeout(() => {
-    const recipeID = [543, 795, 213, 198];
-    console.log(recipeID);
+///////////////////////////////////////////////////////////
+// The Old Way: Asynchronous JavaScript with Callbacks
 
-    setTimeout((id) => {
-      const recipe = {
-        title: 'Fresh Tomato pasta',
-        publisher: 'Jonas'
-      };
-      console.log(`${id}: ${recipe.title}`);
+// const getRecipe = () => {
+//   setTimeout(() => {
+//     const recipeID = [543, 795, 213, 198];
+//     console.log(recipeID);
+//
+//     setTimeout((id) => {
+//       const recipe = {
+//         title: 'Fresh Tomato pasta',
+//         publisher: 'Jonas'
+//       };
+//       console.log(`${id}: ${recipe.title}`);
+//
+//       setTimeout(publisher => {
+//         const recipe2 = {
+//           title: 'Italian Pizza',
+//           publisher: 'Jonas'
+//         };
+//         console.log(recipe2);
+//       }, 1500, recipe.publisher)
+//
+//     }, 1000, recipeID[2]);
+//   }, 1500);
+// }
+// getRecipe();
 
-      setTimeout(publisher => {
-        const recipe2 = {
-          title: 'Italian Pizza',
-          publisher: 'Jonas'
-        };
-        console.log(recipe2);
-      }, 1500, recipe.publisher)
 
-    }, 1000, recipeID[2]);
-  }, 1500);
-}
-getRecipe();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
