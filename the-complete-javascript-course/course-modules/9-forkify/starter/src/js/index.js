25d100006dfc924061e3e9a7731879feb8cb1fc3
Raphael Oliveira <raphaelbrf@gmail.com>
2019-01-30 20:48:40 -0200

    controler

--- the-complete-javascript-course/course-modules/9-forkify/starter/src/js/index.js
+++ the-complete-javascript-course/course-modules/9-forkify/starter/src/js/index.js
@@ -1,17 +1,10 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-const APIKEY = `60241fc7b6065228e497db8ce46ef95c`;
-const URL = `https://www.food2fork.com/api/search`;
 
-async function getRecipesResults(query) {
-  try {
-    const getRecipes = await axios(`${URL}?key=${APIKEY}&q=${query}`);
-    const recipes = getRecipes.data.recipes;
-    console.log(recipes);
-  } catch (error) {
-    console.log(`${error}`);
-  }
+const search = new Search('pizza');
+console.log(search);
 
-}
-getRecipesResults('pizza');
-// Jalapeno
+const recipes = search.getRecipesResults();
+recipes.then(response => {
+  console.log(response);
+})
