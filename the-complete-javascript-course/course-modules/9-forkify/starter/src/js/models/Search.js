d0691a3846a930d682884d1ede525eb491c5d688
Raphael Oliveira <raphaelbrf@gmail.com>
2019-01-30 20:48:47 -0200

    model search

---
+++ the-complete-javascript-course/course-modules/9-forkify/starter/src/js/models/Search.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+
+  async getRecipesResults() {
+    const APIKEY = `60241fc7b6065228e497db8ce46ef95c`;
+    const URL = `https://www.food2fork.com/api/search`;
+    try {
+      const getRecipes = await axios(`${URL}?key=${APIKEY}&q=${this.query}`);
+      this.recipes = getRecipes.data.recipes;
+    } catch (error) {
+      console.log(`${error}`);
+      alert(error);
+    }
+  }
+}
