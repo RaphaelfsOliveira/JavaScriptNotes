37cf841f95860788834d054d7897279fc3f78f32
Raphael Oliveira <raphaelbrf@gmail.com>
2019-01-30 19:39:20 -0200

    clean files

--- the-complete-javascript-course/course-modules/9-forkify/starter/src/js/index.js
+++
@@ -1,6 +0,0 @@
-// Global app controller
-import string from './models/Search';
-// import {add as a, mult as m, ID} from './views/searchView';
-import * as searchView from './views/searchView';
-
-console.log(`${string} ${searchView.add(searchView.ID, 2)} ${searchView.mult(2, searchView.ID)}`);
